a96688f01426d409914aa8f632a6eebdec824900
Ruslan1493 <star93@list.ru>
2018-12-20 11:06:32 +0200

    fix for script files v2

--- script/script.js
+++ script/script.js
@@ -1,8 +1,8 @@
 function redirect(page) {
     if (page === 'strength') {
-        location.href = "../pages/strength.html";
+        location.href = "{{ site.baseurl }}/pages/strength.html";
     } else if (page === 'fat loss') {
-        location.href = "../pages/fat_loss.html";
+        location.href = "{{ site.baseurl }}/pages/fat_loss.html";
     } else if (page === 'condition') {
         location.href = "../pages/conditioning.html";
     }
